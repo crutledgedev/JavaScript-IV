747d36d9b508ddae3a865398169534ebcc036af2
Chance Rutledge <chancerutledge2@gmail.com>
2019-10-17 20:14:14 -0700

    partial commit more to follow

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,139 @@
 // CODE here for your Lambda Classes
+
+
+class Person{ //base class
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }//end constructor
+    speak(){
+        return `Hello my name is ${this.name} I am from ${this.location}`;
+    };
+
+}//end Person
+
+class Instructor extends Person{
+    constructor(attributes){
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject){
+        return `${student} receives a perfect score on ${subject}`;
+    }
+    
+
+}// end Instructor
+
+class Student extends Person{
+    constructor(attributes){
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.classname = attributes.classname;
+        this.favSubjects = attributes.favSubjects;
+
+        
+    }
+    
+        listsSubjects(){
+            console.log(favSubjects);
+        }
+
+          
+    
+        PRAssignments(subject){
+            return `${this.name} has submitted a PR for ${subject}`};
+            
+        sprintChallenge(subject){
+            return `${this.name} has begun sprint challenge on ${subject}`
+        }    
+
+ }//end Instructor
+
+ class ProjectManager extends Instructor{
+     constructor(attributes){
+         super(attributes);
+         this.gradClassName = attributes.gradClassName;
+         this.favInstructor = attributes.favSubjects;
+
+     }
+     standUp(name, channel){
+         `${name} announces to ${channel} @channel standy times`;
+
+     }
+     debugsCode(name, student, subject){
+         return `${this.name} debugs ${student.name}'s code on ${subject}`;
+     }
+ }
+
+
+ 
+
+
+
+ 
+
+
+  const allie = new Instructor({
+    name: "Allie",
+    age: 25,
+    location: "Illinois",
+    //person   
+    
+    specialty:  "Being Awesome",
+    favLanguage: "All of them",
+    catchPhrase: "Why did I think double shifts was a good idea?",
+    //Instructor
+
+    gradClassName: "Web 9",
+    favInstructor:  "Josh Knell",
+    //ProjectManager
+        
+    
+  });
+  
+
+  const chance = new Student({
+    name: "Chance",
+    age: 47,
+    location: "California",
+    //person   
+    previousBackground: "DVD and Software Testing",
+    classname: "Web 25",
+    favSubjects:  "HTML, CSS, Lunch"//array
+    
+ 
+        
+    
+  });
+ 
+
+  const fred = new ProjectManager({
+    name: "Fred",
+    age: 30,
+    location: "Florida",
+    //person   
+    
+    specialty:  "Inline-Block",
+    favLanguage: "Angular",
+    catchPhrase: "Trust The Process",
+    //Instructor
+
+    gradClassName: "Web 15",
+    favInstructor:  "Josh Knell",
+    //ProjectManager
+        
+    
+  });
+  console.log(fred.speak());
+  
+  console.log(chance.speak());
+  console.log(allie.grade("John", "CSS"));
+  console.log(allie.demo("the dangers of over commiting to our schedule"));
+  console.log(allie.speak());
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -23,40 +23,43 @@ Prototype Refactor
   * name
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
+*/  //end GameObject
+  //GameObject prototype method
 
-function GameObject(attributes){
+class GameObject{
+    constructor(attributes){
     this.createdAt = attributes.createdAt;
     this.name = attributes.name;
     this.dimensions = attributes.dimensions;
-    //console.log(this);
-  }//end GameObject
-  //GameObject prototype method
-  GameObject.prototype.destroy = function(){
+    }    
+
+  destroy(){
     return `${this.name} was removed from the game.`;
-  };
+  }
+}
+
   
   /*
     === CharacterStats ===
     * healthPoints
     * takeDamage() // prototype method -> returns the string '<object name> took damage.'
     * should inherit destroy() from GameObject's prototype
+    * //* should inherit destroy() from GameObject's prototype
   */
-  function CharacterStats(attributes){
-    GameObject.call(this, attributes);
-    this.healthPoints = attributes.healthPoints;
+  class CharacterStats extends GameObject{
+      constructor(attributes) {
+        super(attributes);
+        this.healthPoints = attributes.healthPoints;
+    } 
     
-    //* should inherit destroy() from GameObject's prototype
-    
+    takeDamage(){
+        return `${this.name} took damage.`;
+
+    };
   
-    //console.log(this);
+    
   }//end CharacterStats
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  CharacterStats.prototype.takeDamage = function(){
-    return `${this.name} took damage.`;
-  };
-  
+
   /*
     === Humanoid (Having an appearance or character resembling that of a human.) ===
     * team
@@ -67,20 +70,24 @@ function GameObject(attributes){
     //* should inherit takeDamage() from CharacterStats
   
   */
-  function Humanoid(attributes){
-    GameObject.call(this, attributes);
-    CharacterStats.call(this, attributes);
+  class Humanoid extends CharacterStats {
+    constructor(attributes){
+    super(attributes);
     this.team = attributes.team;
     this.weapons = attributes.weapons;
     this.language = attributes.language;
   
-    //console.log(this);
-    }//end Humanod
-    Humanoid.prototype = Object.create(CharacterStats.prototype);
+    };
+    greet(){
+      return `${this.name} offers a greeting in ${this.language}`
+
+    };
+  }//end Humanod
+    //Humanoid.prototype = Object.create(CharacterStats.prototype);
   
-    Humanoid.prototype.greet = function(){
-    return `${this.name} offers a greeting in ${this.language}`;
-    }; 
+    // Humanoid.prototype.greet = function(){
+    // ;
+    // }; 
   /*
     * Inheritance chain: GameObject -> CharacterStats -> Humanoid
     * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
